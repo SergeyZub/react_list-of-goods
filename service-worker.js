b7adec4a9c54f2e6ff3f53ff3a6f8bd67f7e5d3c
Sergey Zub <vook1996@gmail.com>
2021-05-25 20:34:16 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.e28c981ec726fb95eb93181cab1ee8e5.js"
 );
 
 self.addEventListener('message', (event) => {
